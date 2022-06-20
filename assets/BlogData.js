9cabace4302c262df43ab9d113f3d4da50560c70
Emran Hossain <107532051+emranhossain197@users.noreply.github.com>
2022-06-20 06:17:05 -0700

    Add files via upload

---
+++ assets/BlogData.js
@@ -0,0 +1,48 @@
+import P1 from '../assets/images/blog-img1.png'
+import P2 from '../assets/images/blog-img2.png'
+
+
+export const BlogData = [
+    {
+        Name: "TECHNOLEGY",
+        title: 'A small business is only as good as its tools and it is totally true.',
+        plablishDate: "19 Jan 2022",
+        PlablishParson: "John Doe",
+        image: P1
+    },
+    {
+        Name: "WEBNIER",
+        title: 'Colaboration with team members that will be grow to business.',
+        plablishDate: "19 Jan 2022",
+        PlablishParson: "John Doe",
+        image: P2
+    },
+    {
+        Name: "TECHNOLEGY",
+        title: 'A small business is only as good as its tools and it is totally true.',
+        plablishDate: "19 Jan 2022",
+        PlablishParson: "John Doe",
+        image: P1
+    },
+    {
+        Name: "WEBNIER",
+        title: 'Colaboration with team members that will be grow to business.',
+        plablishDate: "19 Jan 2022",
+        PlablishParson: "John Doe",
+        image: P2
+    },
+    {
+        Name: "TECHNOLEGY",
+        title: 'A small business is only as good as its tools and it is totally true.',
+        plablishDate: "19 Jan 2022",
+        PlablishParson: "John Doe",
+        image: P1
+    },
+    {
+        Name: "WEBNIER",
+        title: 'Colaboration with team members that will be grow to business.',
+        plablishDate: "19 Jan 2022",
+        PlablishParson: "John Doe",
+        image: P2
+    }
+]
---
+++ assets/data.js
@@ -0,0 +1,31 @@
+import P1 from '../assets/images/project1.png'
+import P2 from '../assets/images/project2.png'
+import P3 from '../assets/images/project3.png'
+import P4 from '../assets/images/project4.png'
+
+export const Projects = [
+    {
+        name: 'Talent.com - Logo, Full Redesign and Branding',
+        image: P1,
+        description: "Talent.com a new brand identity using their core values of optimism, transparency and approachability as inspiration. As a result, we wereable to create the most streamlined",
+        catagorie: "UX"
+    },
+    {
+        name: 'Bonfire - Landing Page Design Webby Awards Nomination',
+        image: P2,
+        description: "Talent.com a new brand identity using their core values of optimism, transparency and approachability as inspiration. As a result, we wereable to create the most streamlined",
+        catagorie: "UX"
+    },
+    {
+        name: 'Talent.com - Logo, Full Redesign and Branding',
+        image: P3,
+        description: "Talent.com a new brand identity using their core values of optimism, transparency and approachability as inspiration. As a result, we wereable to create the most streamlined",
+        catagorie: "Web Design"
+    },
+    {
+        name: 'Bonfire - Landing Page Design Webby Awards Nomination',
+        image: P4,
+        description: "Talent.com a new brand identity using their core values of optimism, transparency and approachability as inspiration. As a result, we wereable to create the most streamlined",
+        catagorie: "Web Design"
+    }
+]
