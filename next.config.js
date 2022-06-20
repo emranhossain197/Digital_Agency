5306486e50e4bc04704406e58b9d7b3dd7f6c7ba
Emran Hossain <107532051+emranhossain197@users.noreply.github.com>
2022-06-20 06:25:15 -0700

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,30 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+}
+
+module.exports = nextConfig
+
+// const withPlugins = require("next-compose-plugins");
+// const withImages = require("next-images");
+// const webpack = require("webpack");
+// module.exports = withPlugins([withImages], {
+//   webpack: (config) => {
+//     config.node = {
+//       fs: "empty",
+//     };
+//     config.module.rules.push({
+//       test: /\.(glb|gltf|bin)$/,
+//       use: {
+//         loader: "file-loader",
+//       },
+//     });
+//     config.plugins.push(
+//       new webpack.ProvidePlugin({
+//         $: "jquery",
+//         jQuery: "jquery",
+//         "window.jQuery": "jquery",
+//       }));
+//     return config;
+//   },
+// });
