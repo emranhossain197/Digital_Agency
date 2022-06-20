68186e3e4e12849fe923f585fdb728b5cea03a39
Emran Hossain <107532051+emranhossain197@users.noreply.github.com>
2022-06-20 06:16:07 -0700

    Add files via upload

---
+++ components/layout/Layout.js
@@ -0,0 +1,15 @@
+import Navber from '../Navbar'
+import Footer from '../footer'
+import React from 'react'
+
+const Layout = ({ children }) => {
+  return (
+    <div>
+
+      {children}
+
+    </div>
+  )
+
+}
+export default Layout;
